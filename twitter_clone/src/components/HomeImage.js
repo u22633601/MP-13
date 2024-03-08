ff994e03ee95b0727c219408134b10eae01fc356
Quintin <u21513768@tuks.co.za>
2024-03-08 09:01:19 +0200

    Basic Splash page
    Added the general layout and functionality of splash page

---
+++ twitter_clone/src/components/HomeImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styling/HomeImage.css';
+const HomeImage = () => {
+  return (
+    <div className='image-container'>
+        <img src='Twitter.jpg' alt="Twitter home/splash page" className='full-image' />
+    </div>
+  );
+};
+
+export default HomeImage;
