fe498c42928e2c1ea1ac895da1515e4faa548da1
Quintin <u21513768@tuks.co.za>
2024-03-09 21:57:45 +0200

    Added date picker
    Updated temporarily to built-in date picker

--- twitter_clone/src/components/signup/SignUpForm.js
+++ twitter_clone/src/components/signup/SignUpForm.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const SignUpForm = ({ onSubmit }) => {
     const [name, setName] = useState('');
     const [emailOrPhone, setEmailOrPhone] = useState('');
     const [isEmail, setIsEmail] = useState(true);
-    const [month, setMonth] = useState('');
-    const [day, setDay] = useState('');
-    const [year, setYear] = useState('');
+    const [selectedDate, setSelectedDate] = useState(null);
 
     const handleToggle = () => {
         setIsEmail(!isEmail);
         setEmailOrPhone('');
     };
 
+    const handleChange = (date) => {
+        setSelectedDate(date);
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        onSubmit({ name, emailOrPhone, month, day, year });
+        onSubmit({ name, emailOrPhone, selectedDate });
     };
 
     return (
@@ -41,33 +45,7 @@ const SignUpForm = ({ onSubmit }) => {
             </button>
             <label>
                 Date of Birth:
-                <select value={month} onChange={e => setMonth(e.target.value)} required>
-                    <option value="" disabled>Month</option>
-                    <option value="1">January</option>
-                    <option value="2">February</option>
-                    <option value="3">March</option>
-                    <option value="4">April</option>
-                    <option value="5">May</option>
-                    <option value="6">June</option>
-                    <option value="7">July</option>
-                    <option value="8">August</option>
-                    <option value="9">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                </select>
-                <select value={day} onChange={e => setDay(e.target.value)} required>
-                    <option value="" disabled>Day</option>
-                    {Array.from({ length: 31 }, (_, i) => i + 1).map(day => (
-                        <option key={day} value={day}>{day}</option>
-                    ))}
-                </select>
-                <select value={year} onChange={e => setYear(e.target.value)} required>
-                    <option value="" disabled>Year</option>
-                    {Array.from({ length: 125 }, (_, i) => 1900 + i).map(year => (
-                        <option key={year} value={year}>{year}</option>
-                    ))}
-                </select>
+                <DatePicker selected={selectedDate} onChange={handleChange} />
             </label>
             <input type="submit" value="Sign Up" />
         </form>
